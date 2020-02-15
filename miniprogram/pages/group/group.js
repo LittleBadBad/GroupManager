bd1a058e9771f54d23a330a069e525b515b03030
littlebad <1287299719@qq.com>
2020-02-15 14:40:41 +0800

    。。。
    。。。

--- miniprogram/pages/group/group.js
+++ miniprogram/pages/group/group.js
@@ -10,6 +10,7 @@ Page({
     groupid:'',
     groupsum:'',
     ischairman:false,
+    myauth:'',
     avatarUrl:''
   },
 
@@ -17,33 +18,95 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
-    this.setData({
-      userOpenid:options.pid,
-      groupid:options.gid,
-      avatarUrl:options.avatar
+    var that=this
+    const eventChannel = this.getOpenerEventChannel()
+    eventChannel.on('getData',function(data){
+      wx.setNavigationBarTitle({
+        title: data.group.name,
+      })
+      that.setData({
+        userOpenid:data.pid,
+        groupid:data.group.gid,
+        myauth:data.group.auth,
+        groupsum:data.group.intro,
+        avatarUrl:data.group.avatar
+      })
+      if(data.group.status=='主席')
+        that.setData({
+          ischairman:1
+        })
     })
-    wx.setNavigationBarTitle({
-      title: options.gname,
-    })
+    console.log(this.data)
+  },
 
-    /*5. 发送gid和pid请求此人在群中的身份和群简介
-    *发送：pid，gid
-    *返回：status（此人在群中的身份），intro（群简介）
-    */
-    wx.request({
-      url: 'url',
+  toMembers(){
+    var url='../members/groupMember/groupMember?gid='+this.data.groupid+'&&pid='+this.data.userOpenid+'&&ischairman='+this.data.ischairman+'&&auth='+this.data.myauth
+    wx.navigateTo({
+      /*页面跳转：打 ‘../’即可查看目录结构*/
+      url:url,
       complete: (res) => {},
-      data: {
-        gid:this.data.groupid,
-        pid:this.data.userOpenid
-      },
       fail: (res) => {},
-      method: 'GET',
       success: (result) => {
-        //在此根据身份决定是否显示右上角解散按钮
+        console.log(url)
       },
     })
   },
 
+  dismiss(){
+    wx.showModal({
+      cancelColor: 'green',
+      cancelText: '我再想想',
+      complete: (res) => {
+        console.log(res)
+      },
+      confirmColor: 'red',
+      confirmText: '残忍解散',
+      content: '确认解散你的群？',
+      fail: (res) => {},
+      showCancel: true,
+      success: (result) => {
+        /*发送群id更改群的运行/解散信息
+        *发送：gid
+        *返回：flag（解散成功1失败0）
+         */
+        if(result.confirm)
+          wx.request({
+            url: 'http://st.titordong.cn/DeleteTeam?gid='+this.data.groupid,
+            complete: (res) => {},
+            fail: (res) => {},
+            success: result => {
+              console.log(result.data.flag)
+              if(result.data.flag){
+                let pages=getCurrentPages()
+                let prevPage=pages[pages.length-2]
+                //prevPage.refreshGroup()//刷新前界面
+                var grouplist=prevPage.data.grouplist
+                for (let i = 0; i < grouplist.length; i++) {
+                  if(grouplist[i].gid==this.data.groupid)
+                    grouplist[i].dismissed=1
+                }
+                prevPage.setData({
+                  grouplist:grouplist
+                })
+
+                //Tabbar界面跳转
+                wx.switchTab({
+                  url: '../index/index',
+                  complete: (res) => {console.log('complete',res)},
+                  fail: (res) => {console.log('fail',res)},
+                  success: (res) => {
+                    wx.showToast({
+                      title: '解散成功',
+                      icon:'success'
+                    })
+                    console.log(res)
+                  },
+                })
+              }
+            },
+          })
+      },
+      title: '群解散',
+    })
+  }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,30 +4,31 @@ var util = require('../../utils/util.js');
 Page({
   data: {
     //../../images/blablabl.jpg
-    avatarUrl: '../../images/user-unlogin.png',
+    authdialog:0,
     userInfo: {},
-    logged: false,
-    authorized:false,
-    takeSession:false,
-    noGroup:0,
-    gender:1,
-    username:'none',
-    userOpenid:'qwert',
+    userOpenid:'',
+    groupavatar:'../../images/2.jpg',
+    grouplistsearch:[],
     grouplist: [{
       gid:'1287299719',
       avatarUrl:'../../images/user-unlogin.png',
       name: '校学生会',
       sum: '校会成立于1998年……',
-      num:200
+      num:200,
+      joined:0
     },
     {
       gid:'108672146',
       avatarUrl: '../../images/user-unlogin.png',
       name:'小坏坏',
       sum:'坏坏成员每天至少发20张色图才能完成业绩',
-      num:20
+      num:20,
+      joined:0
     }
-    ]
+    ],
+
+    canvasImg:''
+
   },
 
   //在此登录
@@ -39,152 +40,60 @@ Page({
       return
     }
 
-    //先定义再setdata
-    var userOpenid=this.data.userOpenid
-    wx.cloud.callFunction({
-      name:'login',
-      success:res=>{
-        this.setData({
-        userOpenid:res.result.openid
-      })
-      //调取本地全局数据要加this
-      console.log('userOpenid:',this.data.userOpenid)
-      },
-      fail:res=>{
-      }
-    })
-    
-    /*1. 用openid请求用户的加入社团信息
-    *发送：pid
-    *返回：flag（查询状态1有0无），grouplist（gid，gname，intro，num（人数），avatar（社团头像链接））
-    */
-    wx.request({
-        url: 'http://st.titordong.cn/GetGroup_byPid?pid='+this.data.userOpenid,
-        complete: (res) => {},
-        fail: (res) => {
-          this.setData({
-            authorized:false
-          })
-        },
-        success: (result) => {
-          console.log('login data:',result)
-          if(result.data.flag){
-            this.setData({
-              authorized:true,
-              //设置用户头像昵称等信息
-              //获取用户所加入群组信息
-            })
-            if(result.data.grouplist){
-            this.setData({
-              noGroup:0,
-              grouplist:result.data.grouplist
-            })
-          }
-            else
-            this.setData({
-              noGroup:1
-            })
-          }
-            else
-            this.setData({
-              authorized:false
-            })
-        },
-      })
 },
 
-//在此授权（入库）
-authorize(res){
+onShow(){
+  if(this.data.userOpenid)
+    this.login(this.data.userOpenid)
+  // wx.getImageInfo({
+  //   src: this.data.groupavatar,
+  //   complete: (res) => {},
+  //   fail: (res) => {},
+  //   success: (result) => {
+  //     console.log(result)
+  //   },
+  // })
+},
 
-  for (let i = 0; i < this.data.grouplist.length; i++) {
-    wx.downloadFile({
-      url: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJRfqR4B618pZEK7E1OlsWCn4iaWB0vWKtMJ0vdx4BCriaZ9jQiboTx0nloJuTPCVGqKywBgnkNjdO2g/132.jpg',
-      complete: (res) => {},
-      fail: (res) => {console.log('fail',res)},
-   
-      success: (result) => {
-        console.log('success',result.tempFilePath[0])
-      },
-    })
-  }
-
-
-  console.log('res:',res)
-  if (res.detail.userInfo){
-   // 获取用户信息
-    this.setData({
-      username:res.detail.userInfo.nickName,
-      gender:res.detail.userInfo.gender,
-      avatarUrl:res.detail.userInfo.avatarUrl,
-      authorized:1
-    })
-  //授权完成后直接登录
+onReady(){
+  //先定义再setdata
   wx.cloud.callFunction({
-    name:'login',
-    success:res=>{
-      this.setData({
-      userOpenid:res.result.openid
-    })
-    },
-    fail:res=>{
-      console.log('tfghdtf')
-    }
-  })
-  /*2. 注册入库
-  *发送 pid，pname，pgender，regtime
-  *返回 flag（插入成功提示1成功0失败）
-  */
- var time = util.formatTime(new Date())
-  wx.request({
-    url: 'http://st.titordong.cn/Register?pid='+this.data.userOpenid+'&&pname='+this.data.username+'&&regtime='+time+'&&pgender='+this.data.gender,
-    complete: (res) => {console.log('complete')},
-    fail: (res) => {
-      console.log(res)
-    },
-    method: 'GET',
-    success: (result) => {
-      this.setData({
-        noGroup:1
-      })
-      console.log('result',result)
-      if(result.data.flag)
-      wx.showToast({
-        title: '授权成功！',
-      })
-      else
-      wx.showToast({
-        title: '授权失败',
-        icon:'none'
-      })
-    }
-  })
-  /*3. 上传用户头像
-  *发送：用户头像文件（pid.jpg）
-  *返回：更新状态（1成功0失败）
-   */
-  wx.uploadFile({
-    filePath: this.data.avatarUrl,
-    name: this.data.userOpenid+'.jpg',
-    url: 'www.baidu.com',
-    complete: (res) => {},
-    fail: (res) => {console.log(res)},
-    success: (result) => {
-      console.log(result)
-    },
-  })
-}else{
-wx.showToast({
-  title: '尚未授权',
-  icon:'none'
-})
-}
+  name:'login',
+  success:res=>{
+    this.setData({userOpenid:res.result.openid})
+    //调取本地全局数据要加this
+    console.log('userOpenid:',this.data.userOpenid)
+    this.login(this.data.userOpenid)
   },
+  fail:res=>{console.log(res)}
+})
+},
+
+toggleAuth(){
+  wx.showToast({
+    title: '前往个人界面继续授权'
+  })
+  this.setData({
+    authdialog:0
+  })
+},
 
 toMyGroup:function(event){
+  let i=event.currentTarget.dataset.index
+  //建立缓存
+  // wx.setStorageSync('groupsum', this.data.grouplist[i].Intro)
+  // wx.setStorageSync('groupavatar', this.data.grouplist[i].Avatar)
+
     //跳转传值
-    let i=event.currentTarget.dataset.index
     wx.navigateTo({
-      url:'../group/group?gname='+this.data.grouplist[i].name+'&&gid='+this.data.grouplist[i].gid+'&&pid='+this.data.userOpenid+'&&avatar='+this.data.grouplist[i].avatarUrl
+      url:'../group/group',
+      success:res=>{
+        res.eventChannel.emit('getData',{
+          pid:this.data.userOpenid,
+          group:this.data.grouplist[i]
+        })
+      }
+
     })
   },
 
@@ -221,25 +130,69 @@ clearInput: function () {
 
 inputTyping: function (e) {
   let inputV=e.detail.value
-  /*4. 发送群相关信息搜索相关群
-  *发送：ginfo
-  *返回：grouplist（gid，gname，intro，num（人数），avatar（社团头像链接））
-  */
+  this.setData({
+    inputVal:inputV
+  })
+  //群搜索请求
   wx.request({
-    url: 'url',
+    url: 'http://st.titordong.cn/SearchTeam?ginfo='+inputV,
     complete: (res) => {},
-    data: {
-      ginfo:inputV
-    },
-    dataType: dataType,
     fail: (res) => {},
-    header: header,
-    method: method,
-    responseType: responseType,
-    success: (result) => {
-
+    success: result => {
+      console.log('搜索结果',result)
+      //取群交集 O(n^2) *待优化*
+      var grouplistsearch=new Array()
+      grouplistsearch=result.data.grouplist
+      for (let i = 0; i < grouplistsearch.length; i++) {
+        let group=grouplistsearch[i]
+        for (let j = 0; j < this.data.grouplist.length; j++) {
+          if(group.gid == this.data.grouplist[j].gid)
+            grouplistsearch[i].joined=true
+        }
+      }
+      this.setData({
+        grouplistsearch:grouplistsearch
+      })
     },
   })
+},
+
+//申请加入
+joinReq(e){
+  console.log(e)
+  wx.showToast({
+    title: '加入中',
+    icon:'loading'
+  })
+  var gid=this.data.grouplistsearch[e.currentTarget.id].gid
+  var time = util.formatTime(new Date())
+wx.request({
+  url: 'http://st.titordong.cn/Join_Team?pid='+this.data.userOpenid+'&&gid='+gid+'&&jointime='+time,
+  success: result => {
+    console.log('join',result)
+    var grouplist=this.data.grouplist
+    let i=grouplist.length
+    grouplist[i]=new Object()
+    grouplist[i]=this.data.grouplistsearch[e.currentTarget.id]
+    wx.downloadFile({
+      url:grouplist[i].avatar,
+      complete: (res) => {},
+      fail: (res) => {},
+      success: res => {
+        grouplist[i].avatar=res.tempFilePath
+        grouplist[i].done=true
+        wx.showToast({
+          title: '加入成功',
+          icon:'success'
+        })
+        this.setData({
+          tapsearch: 0,
+          grouplist:grouplist
+        })
+      },
+    })
+  },
+})
 },
 
 //群创建模块
@@ -256,95 +209,176 @@ toggleCreate(){
 },
 
   // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
+doUpload: function () {
+  wx.chooseImage({
+    count: 1, // 默认9
+    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+    success (res) {
+      const src = res.tempFilePaths[0]
+      wx.navigateTo({
+        url: '../upload/upload?src='+src
+      })
+    }
+  })
+},
 
-        const filePath = res.tempFilePaths[0]
-        console.log(filePath)
+//提交群信息
+subGroup(e){
+console.log(e)
+var time = util.formatTime(new Date())
+wx.request({
+  url: 'http://st.titordong.cn/CrtTeam?pid='+this.data.userOpenid+'&&gname='+e.detail.value.gname+'&&regtime='+time+'&&intro='+e.detail.value.gintro+'&&fund='+e.detail.value.fund,
+  complete: (res) => {},
+  fail: (res) => {},
+  success:result=> {
+    var gid=result.data.gid
+    //上传群头像
+    wx.uploadFile({
+      filePath: this.data.groupavatar,
+      name: 'cover',
+      url: 'http://st.titordong.cn/update_team_avatar',
+      complete: (res) => {},
+      fail: (res) => {},
+      formData: {
+        gid:gid,
       },
-      fail: e => {
-        console.error(e)
-      }
+      header: {
+        'content-type': 'multipart/form-data'
+      },
+      success:result=> {
+        console.log('uploadresult',result)
+        wx.showToast({
+          title: '创建成功',
+          icon:'success'
+        })
+        var filePath=this.data.groupavatar
+        //创建成功后刷新界面
+        var grouplist=this.data.grouplist
+        let i=grouplist.length
+        grouplist[i]=new Object()
+        grouplist[i].name=e.detail.value.gname
+        grouplist[i].avatar=filePath
+        grouplist[i].intro=e.detail.value.gintro
+        grouplist[i].status='主席'
+        grouplist[i].num=1
+        grouplist[i].auth='1111111'
+        grouplist[i].gid=gid
+        grouplist[i].done=1
+       this.setData({
+        tapcreate:0,
+        grouplist:grouplist
+      })
+      },
     })
   },
-  
+})
+},
 
-submitgroup(e){
-console.log(e)
-}
-// onGetOpenid: function() {
-  //   // 调用云函数
-  //   wx.cloud.callFunction({
-  //     name: 'login',
-  //     data: {},
-  //     success: res => {
-  //       console.log('[云函数] [login] user openid: ', res.result.openid)
-  //       app.globalData.openid = res.result.openid
-  //       wx.navigateTo({
-  //         url: '../userConsole/userConsole',
-  //       })
-  //     },
-  //     fail: err => {
-  //       console.error('[云函数] [login] 调用失败', err)
-  //       wx.navigateTo({
-  //         url: '../deployFunctions/deployFunctions',
-  //       })
-  //     }
-  //   })
-  // },
+//自定义函数
+login(pid){
+    //登录请求
+    wx.request({
+      url: 'http://st.titordong.cn/Login?pid='+pid,
+      fail:res=>{
+        wx.showToast({
+          title: '登录失败，请重启小程序'
+        })
+      },
+      success: logres => {
+      console.log('login data:',logres)
+      if(logres.data.flag){
+          console.log('已入库')
+          var avatarUrl=logres.data.avatarUrl
+          //下载用户头像
+          wx.downloadFile({
+            url: logres.data.avatarUrl,
+            success: (result) => {
+              avatarUrl=result.tempFilePath
+              //1.设置全局变量
+              var userinfo=new Object()
+              userinfo.name=logres.data.name
+              userinfo.avatarUrl=avatarUrl
+              app.globalData.userinfo=userinfo
+              //2.设置本地变量
+              var grouplist=logres.data.grouplist
+              app.globalData.grouplist=grouplist
+              this.setData({
+                grouplist:grouplist
+              })
+              var that=this
+              setTimeout(function(){that.setData({grouplist:grouplist})},300)
+              //下载群头像
+              for (let i = 0; i < grouplist.length; i++) {
+                var starttime=new Date().getTime()
+                wx.downloadFile({
+                  url: grouplist[i].avatar,
+                  complete: (res) => {},
+                  fail: (res) => {console.log(res)},
+                  success: (result) => {
+                    var endtime=new Date().getTime()
+                    var time=(endtime-starttime)/1000
+                    //console.log((endtime-starttime)/1000,'s')
+                    grouplist[i].done=1//加载完成
+                    grouplist[i].avatar=result.tempFilePath
+                    //console.log('downloadsuccess',grouplist[i].name)
+                    if(time > 0.3)
+                      this.setData({
+                        grouplist:grouplist
+                      })
+                  },
+                })
+              }
+            },
+          })
+      }
+      else{
+        console.log('未入库')
+        this.setData({grouplist:{}})
+        wx.showToast({
+          title: '前往个人页面授权'
+        })
+        }
+      },
+    })
+  },
 
-  // // 上传图片
-  // doUpload: function () {
-  //   // 选择图片
-  //   wx.chooseImage({
-  //     count: 1,
-  //     sizeType: ['compressed'],
-  //     sourceType: ['album', 'camera'],
-  //     success: function (res) {
-
-  //       wx.showLoading({
-  //         title: '上传中',
-  //       })
-
-  //       const filePath = res.tempFilePaths[0]
-        
-  //       // 上传图片
-  //       const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-  //       wx.cloud.uploadFile({
-  //         cloudPath,
-  //         filePath,
-  //         success: res => {
-  //           console.log('[上传文件] 成功：', res)
-
-  //           app.globalData.fileID = res.fileID
-  //           app.globalData.cloudPath = cloudPath
-  //           app.globalData.imagePath = filePath
-            
-  //           wx.navigateTo({
-  //             url: '../storageConsole/storageConsole'
-  //           })
-  //         },
-  //         fail: e => {
-  //           console.error('[上传文件] 失败：', e)
-  //           wx.showToast({
-  //             icon: 'none',
-  //             title: '上传失败',
-  //           })
-  //         },
-  //         complete: () => {
-  //           wx.hideLoading()
-  //         }
-  //       })
-
-  //     },
-  //     fail: e => {
-  //       console.error(e)
-  //     }
-  //   })
-  // },
+//压缩头像
+//独立成模块失败，使用随时复制
+compressImg(imgurl){ 
+  wx.getImageInfo({
+      src: imgurl,
+      success: res =>{
+        //---------利用canvas压缩图片--------------
+        var ratio = 2;
+        var canvasWidth = res.width //图片原始长宽
+        var canvasHeight = res.height
+        while (canvasWidth > 132 || canvasHeight > 132){// 保证宽高在132以内
+            canvasWidth = Math.trunc(res.width / ratio)
+            canvasHeight = Math.trunc(res.height / ratio)
+            ratio++;
+        }
+        //----------绘制图形并取出图片路径--------------
+        var ctx = wx.createCanvasContext('canvas_compress')
+        ctx.drawImage(res.path, 0, 0, canvasWidth, canvasHeight)
+        ctx.draw(false, setTimeout(function(){
+          console.log(that.data.groupavatar)
+             wx.canvasToTempFilePath({
+                 canvasId: 'canvas_compress',
+                 destWidth: canvasWidth,
+                 destHeight: canvasHeight,
+                 success: res=>{
+                    console.log('canvasres',res.tempFilePath)//最终图片路径
+                 },
+                 fail: res=>{
+                     console.log('canvas',res)
+                }
+            })
+        },100))//留一定的时间绘制canvas
+      },
+      fail: res=>{
+      console.log('getImageInfo',res.errMsg)
+      },
+    })
+},
 })
